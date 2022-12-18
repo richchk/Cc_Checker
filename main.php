db7e8865ebbb3eec531831c60c26a2862952d38a
richchk <120972417+richchk@users.noreply.github.com>
2022-12-19 01:58:55 +0800

    Update main.php

--- main.php
+++ main.php
@@ -4,7 +4,7 @@
 
 ///https://api.telegram.org/bot<token>/setwebhook?url=<url>
 
-$botToken = "5261916902:AAFhg0QN0s9weVpPzIZdnNihjQLRSwfXbrk"; // Enter ur bot token
+$botToken = "5853774094:AAGuNsVsvhMhPuqbnhFhsjOeYpAtfgEAVnI"; // Enter ur bot token
 $website = "https://api.telegram.org/bot".$botToken;
 error_reporting(0);
 $update = file_get_contents('php://input');
